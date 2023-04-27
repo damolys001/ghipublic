41263f452d00147634abc5df2580b9bf25ed96c3
Adedamola Adeyemi <adedamola.adeyemi@live.com>
2023-04-27 07:52:54 -0700

    fixing the receipt from

--- forms/contact.php
+++ forms/contact.php
@@ -59,7 +59,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From:Website<webmaster@example.com>' . "\r\n";
+$headers .= 'From:Website<no-reply@ghiassets.com>' . "\r\n";
 
 
 
