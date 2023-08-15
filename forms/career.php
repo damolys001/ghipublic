aab5f8c2fa2a4338d0734c4b34e14f58e3284454
Adedamola Adeyemi <adedamola.adeyemi@live.com>
2023-08-15 02:17:24 +0100

    puting production email

--- forms/career.php
+++ forms/career.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'damola@betwick.com.ng';
+  $receiving_email_address = 'hr@ghiassets.com';
 
 
   $fname = "";
